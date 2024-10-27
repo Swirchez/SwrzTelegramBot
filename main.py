98ce2b20be012d2790b433c781daee505d1a3bc8
Swirchez <anuarkiykov@gmail.com>
2024-10-27 18:10:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ from telebot.types import ReactionTypeEmoji, InlineKeyboardButton, InlineKeyboar
 import os
 import random
 import requests
-API_TOKEN = "<api_token>"
-bot = telebot.TeleBot('<token>')
+bot = telebot.TeleBot('ToKeN')
 facts = ['Собрать мусор на пляже или в парке и правильно его утилизировать.',
           'Сделать компост из органических отходов.', 'Убрать дом и правильно все утилизировать.',
           'Посадить растение на балконе или во дворе.',
           'Принять участие в акции по очистке речек, озер или лесов.',
           'Поддержать инициативы по защите природных территорий и животных.',
           'Провести экологический день, в котором все действия будут направлены на сохранение природы и ее богатства.']
+@bot.message_handler(commands=["start"])
+def help_user(message):
+    bot.send_message(message.chat.id, "Здраствуйте я телеграм бот который даёт советы о том как можно уменьшить загрязнения окружающей среды, пиши команду /help - чтобы узнать все команды бота!")
+
+@bot.message_handler(commands=["help"])
+def help_user(message):
+    bot.send_message(message.chat.id, "Команды: /fact - присылает один рандомный факт об экологии, /facts - присылает два дополнительных факта о экологии, /nquiz - присылает викторину о знаниях про экологию.")
 
 @bot.message_handler(commands=["fact"])
 def fact(message):
@@ -34,4 +40,17 @@ def answer(call):
     elif call.data == '2':
         bot.send_message(call.message.chat.id, 'Сортировать мусор по категориям.')
 
+@bot.message_handler(commands=["nquiz"])
+def create_poll(message):
+    bot.send_message(message.chat.id, "Квиз на знание экологии.")
+    answer_options = ["Чтобы сохранить ресурсы для будущих поклений.", "Чтобы улучшить качество воздуха и воды.", "Чтобы защитить биоразнообразие.", "Чтобы предотвратить климатические изменения."]
+    bot.send_poll(
+        chat_id=message.chat.id,
+        question="Зачем нужно заботиться об окружающей среде?",
+        options=answer_options,
+        type="quiz",
+        correct_option_id=3,
+        is_anonymous=False,
+    )
+    
 bot.infinity_polling()
